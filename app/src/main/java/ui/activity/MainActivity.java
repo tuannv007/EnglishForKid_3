8fdf96e8f842a2b5c0dbd47b9b0f5ba268a47342
tuannv007 <tuan.dev.java@gmail.com>
2016-12-20 11:30:49 +0700

    init dapter

---
+++ app/src/main/java/ui/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.framgia.englishforkid_3.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
